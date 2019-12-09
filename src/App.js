f52b12a4dbe4b2ad78a76d52fcdb581c4ea477ad
jemiks <jelo.kokhodze@gmail.com>
2019-12-09 21:26:38 +0400

    example for show more show less functional

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+import './style.css'
 
 function App() {
+  const [showMore, setShowMore] = useState(false)
+  const [showText, setShowText] = useState(false)
+  const testRef = useRef()
+
+  useEffect(() => {
+    if (testRef.current.scrollHeight > testRef.current.offsetHeight) setShowMore(true)
+  }, [])
+
+  const toggleShowText = () => setShowText((state) => !state)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <p className={showText ? 'show-more' : "show-less"} ref={testRef}>Lorem Ipsum is simply dummy text of the printing
+         and typesetting industry. Lorem Ipsum has
+         been the industry's standard dummy text ever since the 1500s,
+         when an unknown printer took a galley of type and scrambled it t
+       . It has survived not only five centuries,
+          but also the leap into electronic typesetting, remaining essentially unchanged.xstt
+          </p>
+      {showMore && <button onClick={toggleShowText}>read {showText ? 'less' : 'more'}</button>}
     </div>
   );
 }
